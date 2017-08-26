6d34baf953805452750c4b143dbc6a1896efc3b4
adystiadrianne <adystiadrianne@gmail.com>
2017-08-26 21:22:48 +0700

    blabla2

--- ga.py
+++ ga.py
@@ -1,2 +1,28 @@
-#//ini adysti
-#ni cio
+
+import math
+import random
+
+class Node:
+    int x
+    int y
+    def __init__(self, x,y):
+       self.x = x
+       self.y = y
+
+    def getX:
+        return self.x
+
+    def getY:
+        return self.y
+
+    def jarak(self,node):
+        jarX = abs(self.getX() - node.getX())
+        jarY = abs(self.getY() - node.getY())
+        jarXY = math.sqrt(jarX**2 + jarY**2)
+        return jarXY
+
+if __name__ == '__main__':
+
+
+
+
